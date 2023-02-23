f223f416235b1914e640d21953d906da0caf7c6b
DamianRodriguez <rodriguezdamian@hotmail.com>
2023-02-23 15:41:07 -0500

    archivos nuevos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { IngresarComponent } from './routes/ingresar/ingresar.component';
+import { InicioComponent } from './routes/inicio/inicio.component';
+import { PeliculasComponent } from './routes/peliculas/peliculas.component';
+import { SeriesComponent } from './routes/series/series.component';
+
+const routes: Routes = [
+  {
+  path:"inicio",
+  component:InicioComponent
+}
+{
+  path:"peliculas",
+  component:PeliculasComponent
+}
+{
+  path:"series",
+  component:SeriesComponent
+}
+{
+  path:"ingresar",
+  component:IngresarComponent
+}
+{
+  path:"**"
+  redirectTo"inicio"
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from './layout/layout.module';
+import { Routes } from './routes/routes.module';
+import {shared } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
